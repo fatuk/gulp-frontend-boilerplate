48c8b0e22d22341faa98bd331767172745568492
fatuk <fatukk@gmail.com>
2014-12-15 00:45:57 +0300

    Add main tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,95 +7,118 @@ var browserSync = require('browser-sync');
 var minifyCSS = require('gulp-minify-css');
 var iconfont = require('gulp-iconfont');
 var consolidate = require('gulp-consolidate');
+var mainBowerFiles = require('main-bower-files');
+var jsPlugins = mainBowerFiles();
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
 
+/******************************
+ * JS plugins
+ ******************************/
+gulp.task('js', function () {
+	gulp.src(jsPlugins)
+		.pipe(concat('plugins.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js/'));
+});
+
+/******************************
+ * Default task
+ ******************************/
 gulp.task('default', [
-    'browser-sync',
-    'iconfont',
-    'sass',
-    'watch'
+	'browser-sync',
+	'iconfont',
+	'sass',
+	'watch'
 ]);
 
-gulp.task('iconfont', function() {
-    gulp.src(['icons/*.svg'])
-        .pipe(iconfont({
-            fontName: 'myfont'
-        }))
-        .on('codepoints', function(codepoints, options) {
-            gulp.src('templates/myfont.css')
-                .pipe(consolidate('lodash', {
-                    glyphs: codepoints,
-                    fontName: 'myfont',
-                    fontPath: '../fonts/',
-                    className: 's'
-                }))
-                .pipe(gulp.dest('app/css/'));
-        })
-        .pipe(gulp.dest('app/fonts/'));
+/******************************
+ * SVG icon fonts
+ ******************************/
+gulp.task('iconfont', function () {
+	gulp.src(['icons/*.svg'])
+		.pipe(iconfont({
+			fontName: 'myfont'
+		}))
+		.on('codepoints', function (codepoints, options) {
+			gulp.src('templates/myfont.css')
+				.pipe(consolidate('lodash', {
+					glyphs: codepoints,
+					fontName: 'myfont',
+					fontPath: '../fonts/',
+					className: 'icon'
+				}))
+				.pipe(gulp.dest('app/css/'));
+		})
+		.pipe(gulp.dest('app/fonts/'));
 });
 
 
-/**
- *
- *	Browsers sync
- *
- **/
+/******************************
+ * Browser sync
+ ******************************/
+gulp.task('browser-sync', function () {
+	var files = [
+		'app/**/*.html',
+		'app/js/*.js',
+		'app/css/**/*.css'
+	];
 
-gulp.task('browser-sync', function() {
-    var files = [
-        'app/**/*.html',
-        'app/css/**/*.css'
-    ];
-
-    browserSync.init(files, {
-        server: {
-            baseDir: './app'
-        }
-    });
+	browserSync.init(files, {
+		server: {
+			baseDir: './app'
+		}
+	});
 });
 
-
-/**
- *
- *	Watch
- *
- **/
-
-gulp.task('watch', function() {
-    gulp.watch('app/scss/*.scss', ['sass']);
-    gulp.watch('icons/*.svg', ['iconfont']);
+/******************************
+ * Watch
+ ******************************/
+gulp.task('watch', function () {
+	gulp.watch('app/scss/*.scss', ['sass']);
+	gulp.watch('icons/*.svg', ['iconfont']);
 });
 
+/******************************
+ * SASS
+ ******************************/
+gulp.task('sass', function () {
+	gulp.src('app/scss/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			errLogToConsole: true
+		}))
+		.pipe(sourcemaps.write('../css'))
+		.pipe(gulp.dest('app/css'));
 
-
-
-/**
- *
- *	Watch sass
- *
- **/
-
-gulp.task('sass', function() {
-    gulp.src('app/scss/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(sourcemaps.write('../css'))
-        .pipe(gulp.dest('app/css'));
+	setTimeout(function () {
+		gulp.start('autoprefixer');
+	}, 50);
 });
 
+/******************************
+ * Minify css
+ ******************************/
+gulp.task('minify-css', function () {
+	gulp.src('app/css/*.css')
+		.pipe(minifyCSS({
+			keepBreaks: false,
+			keepSpecialComments: true,
+			benchmark: false,
+			debug: true
+		}))
+		.pipe(gulp.dest('app/css/'));
+});
 
-/**
- *
- *	Compress css
- *
- **/
-
-gulp.task('minify-css', ['sass'], function() {
-    gulp.src('app/css/*.css')
-        .pipe(minifyCSS({
-            keepBreaks: false,
-            keepSpecialComments: true,
-            benchmark: false,
-            debug: true
-        }))
-        .pipe(gulp.dest('app/css/'));
+/******************************
+ * Autoprefixer
+ ******************************/
+gulp.task('autoprefixer', function () {
+	gulp.src('app/css/main.css')
+		.pipe(autoprefixer({
+			browsers: ['last 5 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('app/css'));
 });
